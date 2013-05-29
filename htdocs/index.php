71de1e9c03d8c9e890b155bb49e26ee6793a49c0
reuhtte <>
2013-05-29 18:03:32 -0500

    - nueva versión

--- htdocs/index.php
+++ htdocs/index.php
@@ -7,11 +7,15 @@ require_once dirname(__FILE__).'/config.inc.php'
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <base href="<?php echo BASE_URL; ?>" />
         <!--<link rel="icon" href="images/sys/favicon.ico" sizes="16x16" type="image/ico" />-->
-        <link rel="stylesheet" type="text/css" href="css/general.css"/>
+        
+        <link rel="stylesheet" type="text/css" media="(max-device-width: 800px)" href="css/800.css" />
+        <link rel="stylesheet" type="text/css" media="(min-device-width: 801px) and (max-device-width: 1024px)" href="css/1024.css" />
+        <link rel="stylesheet" type="text/css" media="(min-device-width: 1025px)" href="css/general.css"/>
+        
         <link rel="stylesheet" type="text/css" href="css/smoothness/jquery-ui-1.9.2.custom.min.css"/>
         
-        <link href='http://fonts.googleapis.com/css?family=Gilda+Display' rel='stylesheet' type='text/css'>
-        <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+       
+        <link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700|Bevan' rel='stylesheet' type='text/css'>
         
         <title>Contadores P&uacute;blicos Certificados</title>
         <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
@@ -24,40 +28,70 @@ require_once dirname(__FILE__).'/config.inc.php'
         <div id="maincontainer">
             <!-- header -->
             <div id="header">
-                <div style="width: 960px; height: 80px; display: inline-block;">
-                    <br/>
+                
+                <div id="h_main">
                     <div id="h_main_text">CPC V&iacute;ctor Barcenas Y Asociados S.C.</div>
                     <div id="h_submain_text">Contadores P&uacute;blicos Certificados</div>
                 </div>
-                <div style="width: 960px; display: inline-block;">
-                    <div id="h_main_menu">
-                        <ul>
-                            <li>
-                                <a href="">Inicio</a>
-                            </li>
-                            <li>
-                                <a href="somos/">Quienes Somos</a>
-                            </li>
-                            <li>
-                                <a href="servicios/">Servicios</a>
-                            </li>
-                            <li>
-                                <a href="clientes/">Nuestros Clientes</a>
-                            </li>
-                            <li>
-                                <a href="links/">Links</a>
-                            </li>
-                            <li>
-                                <a href="contacto/">Cont&aacute;ctanos</a>
-                            </li>
-                        </ul>
-                    </div>
+                <div id="h_main_menu">
+                    <ul>
+                        <li class="mm_element mm_selected">
+                            <a href="">Inicio</a>
+                        </li>
+                        <li class="mm_element mm_margin_left">
+                            <a href="somos/">Quienes Somos</a>
+                        </li>
+                        <li class="mm_element mm_margin_left">
+                            <a href="servicios/">Servicios</a>
+                        </li>
+                        <li class="mm_element mm_margin_left">
+                            <a href="clientes/">Nuestros Clientes</a>
+                        </li>
+                        <li class="mm_element mm_margin_left">
+                            <a href="links/">Links</a>
+                        </li>
+                        <li class="mm_element mm_margin_left">
+                            <a href="contacto/">Cont&aacute;ctanos</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
             <!-- content -->
             <div id="content">
                 <div id="subcontent">
-                    <div class="info_panel">
+                    
+                    <div style="border-top: 3px solid rgb(227,95,95); background-color: #ccc; font-family: 'Bevan', cursive; font-size: 1.6em; color: rgb(227,95,95); padding: 5px 45px;">
+                        Calidad
+                    </div>
+                    
+                    <div style="background-color: white; border: 0; margin: 0; padding: 0;">
+                        <div style="display: table-cell;">
+                            <img src="/images/gallery/galeria1.png"/>
+                        </div>
+                        <div style="display: table-cell; padding: 10px; vertical-align: top;">
+                            <h3 style="color: #666;">Pol&iacute;tica de Calidad:</h3>
+                            <p>
+                                La organizaci&oacute;n y su Personal nos comprometemos a
+                                satisfacer y superar los requerimientos de nuestros Clientes,
+                                constituy&eacute;ndonos en un Socio Estrat&eacute;gico de sus Negocios.
+                            </p>
+
+                            <h3 style="color: #666;">Objetivos de Calidad:</h3>
+                            <p>
+                                Contribuir al crecimiento de los Negocios de nuestros Clientes.
+                                <br/>
+                                <br/>
+                                Lograr la concientizaci&oacute;n del personal de la importancia de
+                                trabajar con calidad y responsabilidad.
+                                <br/>
+                                <br/>
+                                Lograr la Solidez Financiera de la Organizaci&oacute;n
+                            </p>
+                        </div>
+                    </div>
+                    
+                    
+<!--                    <div class="info_panel">
                         <img src="images/about_us.jpg" width="270"/>
 
                         <div class="slider">
@@ -133,7 +167,7 @@ require_once dirname(__FILE__).'/config.inc.php'
                         <p>
                             <img src="/images/amcp.gif"/>
                         </p>
-                    </div>
+                    </div>-->
                 </div>
             </div>
             <!-- footer -->
--- htdocs/js/general.js
+++ htdocs/js/general.js
@@ -3,23 +3,46 @@
  * and open the template in the editor.
  */
 
+//function adjustStyle(width) {
+//    console.log(width);
+//    width = parseInt(width);
+//    if (width <= 800) {
+//        $("#size-stylesheet").attr("href", "css/800.css");
+//    } else if ((width >= 801) && (width <= 1024)) {
+//        $("#size-stylesheet").attr("href", "css/1024.css");
+//    } else {
+//       $("#size-stylesheet").attr("href", "css/general.css"); 
+//    }
+//}
+
 $(document).ready(function(){
-    $('.slider').css('height','0').hide();
-    $(".info_panel").hover(
-        function(){
-            $(".slider", this).show().animate({height:'50px'});
-        },
-        function(){
-            $(".slider", this).animate({height:0}, function() {
-                $(this).hide();
-            });
-        }   
-    )
+//    $('.slider').css('height','0').hide();
+//    $(".info_panel").hover(
+//        function(){
+//            $(".slider", this).show().animate({height:'50px'});
+//        },
+//        function(){
+//            $(".slider", this).animate({height:0}, function() {
+//                $(this).hide();
+//            });
+//        }   
+//    );
     
 //    $('.info_panel').hover(function(){ 
 //        $(".slider", this).stop().animate({top:'200px'},{queue:false,duration:160}); 
 //    }, function() { 
 //        $(".slider", this).stop().animate({top:'295px'},{queue:false,duration:160}); 
-//    });	  
+//    });
+    
+
+//    $(function() {
+//        adjustStyle($(this).width());
+//        $(window).resize(function() {
+//            adjustStyle($(this).width());
+//        });
+//    });
+
+//    adjustStyle($(window).width());
+
 });
 
